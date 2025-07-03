8beed778220c2ce64309e82f7f1eafcec93372c0
LuisFelipe272009 <luis.piekarski@escola.pr.gov.br>
2025-07-03 10:50:02 -0300

    Add files via upload

--- Times Interclasse 2025 CEAG/interclase.py
+++ Times Interclasse 2025 CEAG/interclase.py
@@ -3,4 +3,5 @@ partidas = list()
 jogador ["nome"] = str(input('nome do jogador: '))
 tot = int(input(f'quantas partidas{jogador["nome"]} jogou ?'))
 for c in range(0,tot):
-    partidas.append(int(input(f'   quantos gols n partida {c}?')))
+    partidas.append(int(input(f'   quantos gols na partida {c}?')))
+    jogador["gols"]
